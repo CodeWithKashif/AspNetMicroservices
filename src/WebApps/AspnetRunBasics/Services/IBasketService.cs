297d32d1a42b763226ea9a2a865ea10a93f41f66
kreza <kashif.reza@icreon.com>
2022-02-10 18:55:13 +0530

    webapps work completed end to end

---
+++ src/WebApps/AspnetRunBasics/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+using AspnetRunBasics.Models;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/IOrderService.cs
@@ -0,0 +1,12 @@
+using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+
+}
